ee1adf29c9e17a56ed5e9a1f8f69037e5c33759d
Elwick1 <michaelrwilliamson92@gmail.com>
2021-01-14 19:20:00 +0000

    button to show/hide posts via a state change"

--- src/App.js
+++ src/App.js
@@ -12,16 +12,27 @@ import Posts from './Posts';
 
 export default class App extends React.Component {
 
+  state = {
+    showPosts : false
+  };
   
+  handleClick = () => {
+    this.setState({
+      showPosts : !this.state.showPosts
+    })
+  }
 
   render() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="proof"> ITS WORKING </h1>
+        <button className="showBtn" onClick={this.handleClick}> Show Posts </button>
+        {this.state.showPosts === true ? (
         <div className="posts-container">
           <Posts/>
         </div>
+        ) : null }
       </header>
     </div>
   );
